55e100450c030b357470be45a30edd52a387f471
yj00114.jang <yjj00114@gmail.com>
2025-03-28 10:41:12 +0900

    step 1 - check length tc pass

---
+++ SimilarityCheck/similarity_check.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using std::string;
+
+class SimilarityChecker
+{
+public:
+	int checkLength(string str1, string str2)
+	{
+		return 60;
+	}
+};
--- SimilarityCheck/similarity_check_test.cpp
+++ SimilarityCheck/similarity_check_test.cpp
@@ -3,6 +3,12 @@
 
 using namespace testing;
 
+TEST(SimilarityCheck, length_check_tc)
+{
+	SimilarityChecker checker;
+	EXPECT_EQ(checker.checkLength("qwe", "wer"), 60);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
